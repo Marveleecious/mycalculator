f6953b674416b28e7b1eb796c2e285fb7f9a92f2
Marveleecious <richardtimilehin05@gmail.com>
2022-10-02 20:01:32 -0700

    first statement

---
+++ script.js
@@ -0,0 +1,120 @@
+function one(){
+    let output = document.getElementById('output');
+    output.value +=1
+    // console.log(output)
+}
+
+function two(){
+    let output = document.getElementById('output');
+    output.value +=2
+    // console.log(output)
+}
+
+function three(){
+    let output = document.getElementById('output');
+    output.value +=3
+    // console.log(output)
+}
+
+function four(){
+    let output = document.getElementById('output');
+    output.value +=4
+    // console.log(output)
+}
+
+function five(){
+    let output = document.getElementById('output');
+    output.value +=5
+    // console.log(output)
+}
+
+function six(){
+    let output = document.getElementById('output');
+    output.value +=6
+    // console.log(output)
+}
+
+function seven(){
+    let output = document.getElementById('output');
+    output.value +=7
+    // console.log(output)
+}
+
+function eight(){
+    let output = document.getElementById('output');
+    output.value +=8
+    // console.log(output)
+}
+
+function nine(){
+    let output = document.getElementById('output');
+    output.value +=9
+    // console.log(output)
+}
+
+function zero(){
+    let output = document.getElementById('output');
+    output.value +=0
+    // console.log(output)
+}
+
+function percent(){
+    let output = document.getElementById('output');
+    output.value +='%'
+    // console.log(output)
+}
+
+function point(){
+    let output = document.getElementById('output');
+    output.value +='.'
+    // console.log(output)
+}
+
+function plus(){
+    let output = document.getElementById('output');
+    output.value +='+'
+    // console.log(output)
+}
+
+function minus(){
+    let output = document.getElementById('output');
+    output.value +='-'
+    // console.log(output)
+}
+
+function multiply(){
+    let output = document.getElementById('output');
+    output.value +='*'
+    // console.log(output)
+}
+
+function divide(){
+    let output = document.getElementById('output');
+    output.value +='/'
+    // console.log(output)
+}
+
+function square(){
+    let output = document.getElementById('output');
+    output.value +='**'
+    // console.log(output)
+}
+
+// function removing(){
+//     let output = document.getElementById('output');
+//     output.value -=  
+//     // console.log(output)
+// }
+
+function clearing(){
+    let output = document.getElementById('output');
+    output.value = ''
+    // console.log("fgy")
+
+}
+
+function equalto(){
+    let output = document.getElementById('output');
+    let result = eval (output.value);
+    output.value = result;
+}
